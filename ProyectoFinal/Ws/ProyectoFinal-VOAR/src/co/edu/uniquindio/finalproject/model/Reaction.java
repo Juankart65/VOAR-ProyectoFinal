685cc7af3efb1aea8393f413f8036968ac9505fc
Juankart65 <104481229+Juankart65@users.noreply.github.com>
2022-11-24 01:28:04 -0500

    Add files via upload

---
+++ ProyectoFinal/Ws/ProyectoFinal-VOAR/src/co/edu/uniquindio/finalproject/model/Reaction.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.finalproject.model;
+
+/**
+ * This is the list of reactions
+ *
+ * @author Jose Manuel Taborda
+ * @author Juan Esteban Ramirez
+ * @author Juan Esteban Cardona
+ *
+ */
+public enum Reaction {
+	LIKE, DISLIKE;
+}
